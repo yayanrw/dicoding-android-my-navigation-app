42b06840ee4ca15da6126ec7e2fa14d1e44cbf6a
yayan-msglow <yayanrahmat@msglowid.com>
2021-10-31 16:57:11 +0700

    add popup behavior

--- app/src/main/java/com/example/mynavigation/DetailCategoryFragment.kt
+++ app/src/main/java/com/example/mynavigation/DetailCategoryFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.mynavigation.databinding.FragmentDetailCategoryBinding
 
 // TODO: Rename parameter arguments, choose names that match
@@ -42,6 +43,10 @@ class DetailCategoryFragment : Fragment() {
 
         binding.tvCategoryName.text = dataName
         binding.tvCategoryDescription.text = "Stock : $dataDescription"
+
+        binding.btnProfile.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_detailCategoryFragment_to_homeFragment)
+        )
     }
 
     override fun onDestroy() {
