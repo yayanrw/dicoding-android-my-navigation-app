f7d1f7521fe61e3dd1ca363faff550fb440aa574
yayan-msglow <yayanrahmat@msglowid.com>
2021-10-31 12:08:03 +0700

    add detail category

---
+++ app/src/main/java/com/example/mynavigation/DetailCategoryFragment.kt
@@ -0,0 +1,47 @@
+package com.example.mynavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.mynavigation.databinding.FragmentDetailCategoryBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [DetailCategoryFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class DetailCategoryFragment : Fragment() {
+
+    private var _binding: FragmentDetailCategoryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentDetailCategoryBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val dataName = arguments?.getString(CategoryFragment.EXTRA_NAME)
+        val dataDescription = arguments?.getLong(CategoryFragment.EXTRA_STOCK)
+
+        binding.tvCategoryName.text = dataName
+        binding.tvCategoryDescription.text = "Stock : $dataDescription"
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
